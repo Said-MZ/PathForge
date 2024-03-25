1bab161bb1f5ab9c342e9f0e6d4cd14e9fce20af
Said zain <saidzain897@gmail.com>
2024-03-25 15:06:07 +0300

    Add filtering functionality

--- src/components/roadmaps-page/roadmaps-filter.tsx
+++ src/components/roadmaps-page/roadmaps-filter.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import * as React from "react";
 import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
-
+import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
@@ -19,65 +18,78 @@ import {
 } from "@/components/ui/popover";
 
 const roadmaps = [
-  { label: "اللغة العربية", value: "اللغة العربية" },
-  { label: "الرياضيات", value: "الرياضيات" },
-  { label: "العلوم", value: "العلوم" },
-  { label: "التاريخ", value: "التاريخ" },
-  { label: "الجغرافيا", value: "الجغرافيا" },
-  { label: "الفيزياء", value: "الفيزياء" },
+  { id: 1, label: "اللغة العربية", value: "اللغة العربية" },
+  { id: 2, label: "الرياضيات", value: "الرياضيات" },
+  { id: 3, label: "العلوم", value: "العلوم" },
+  { id: 4, label: "التاريخ", value: "التاريخ" },
+  { id: 5, label: "الجغرافيا", value: "الجغرافيا" },
+  { id: 6, label: "الفيزياء", value: "الفيزياء" },
 ];
 
 export function RoadmapsFilter() {
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("");
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState<string>("");
+  const [selectedId, setSelectedId] = useState(0);
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[200px] justify-between"
+    <div className="flex gap-4 justify-center items-center">
+      <Button
+        variant="default"
+        className="w-[200px]"
+        onClick={() => {
+          console.log("value", value);
+          console.log("selectedId", selectedId);
+        }}
+      >
+        تصفية
+      </Button>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger
+          asChild
+          className="flex flex-row-reverse justify-stretch items-center"
         >
-          {value
-            ? roadmaps.find((framework) => framework.value === value)?.label
-            : "الكل"}
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[200px] justify-between pr-2 pl-0"
+          >
+            {value
+              ? roadmaps.find((framework) => framework.value === value)?.label
+              : "الكل"}
 
-          <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <CommandInput
-            placeholder="
-ابحث عن خريطة تعلم"
-            className="h-9"
-          />
-          <CommandEmpty>لا يوجد نتائج للبحث</CommandEmpty>
-          <CommandGroup>
-            {roadmaps.map((framework) => (
-              <CommandItem
-                key={framework.value}
-                value={framework.value}
-                onSelect={(currentValue) => {
-                  setValue(currentValue === value ? "" : currentValue);
-                  setOpen(false);
-                }}
-                className="flex flex-row-reverse justify-between items-between w-full"
-              >
-                {framework.label}
-                <CheckIcon
-                  className={cn(
-                    "ml-auto h-4 w-4",
-                    value === framework.value ? "opacity-100" : "opacity-0"
-                  )}
-                />
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+            <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput placeholder="ابحث عن خريطة تعلم" className="h-9" />
+            <CommandEmpty>لا يوجد نتائج للبحث</CommandEmpty>
+            <CommandGroup>
+              {roadmaps.map((framework) => (
+                <CommandItem
+                  key={framework.value}
+                  value={framework.value}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                    setSelectedId(framework.id);
+                  }}
+                  className="flex flex-row-reverse justify-between items-between w-full"
+                >
+                  {framework.label}
+                  <CheckIcon
+                    className={cn(
+                      "h-4 w-4",
+                      value === framework.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 }
