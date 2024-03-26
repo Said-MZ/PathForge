54cf3ba93584e770123b9d72810e40c10402196e
Said zain <saidzain897@gmail.com>
2024-03-26 16:07:19 +0300

    Add login / signup forms

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,11 +3,13 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Button } from "@/components/ui/button";
 import { IoIosArrowBack } from "react-icons/io";
 import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const LoginPage = () => {
   return (
     <main className="grid place-items-center h-screen w-screen">
-      <Button variant="default" className="absolute top-4 left-4">
+      <Button variant="secondary" className="absolute top-4 left-4">
         <Link
           href="/"
           className="flex justify-center items-center gap-1 hover:gap-2 transition-all"
@@ -16,7 +18,7 @@ const LoginPage = () => {
           <span>الصفحة الرئيسية</span>
         </Link>
       </Button>
-      <Tabs defaultValue="login" className="w-full mx-auto max-w-96">
+      <Tabs defaultValue="login" className="w-full mx-auto max-w-96 min-h-96">
         <TabsList className="w-full">
           <TabsTrigger value="login" className="w-full">
             إنشاء حساب جديد
@@ -26,9 +28,52 @@ const LoginPage = () => {
           </TabsTrigger>
         </TabsList>
         <TabsContent value="login">
-          Make changes to your login here.
+          <form className="flex flex-col gap-4 text-right mt-6">
+            <Label htmlFor="name">الاسم</Label>
+            <Input
+              type="text"
+              id="name"
+              placeholder="الاسم الكامل"
+              required
+              className="text-right"
+            />
+            <Label htmlFor="email">البريد الإلكتروني</Label>
+            <Input
+              type="email"
+              id="email"
+              placeholder="ahmad@example.com"
+              required
+            />
+            <Label htmlFor="password">كلمة المرور</Label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="********"
+              required
+            />
+            <Label htmlFor="confirm-password">تأكيد كلمة المرور</Label>
+            <Input
+              type="password"
+              id="confirm-password"
+              placeholder="********"
+              required
+            />
+            <Button variant="default" className="w-full">
+              إنشاء حساب جديد
+            </Button>
+          </form>
         </TabsContent>
-        <TabsContent value="signup">Change your signup here.</TabsContent>
+        <TabsContent value="signup">
+          <form action="" className="flex flex-col gap-4 text-right mt-6">
+            <Label htmlFor="email">البريد الإلكتروني</Label>
+            <Input type="email" id="email" placeholder="ahmad@example.com" />
+            <Label htmlFor="password">كلمة المرور</Label>
+            <Input type="password" id="password" placeholder="********" />
+            <Button variant="default" className="w-full">
+              تسجيل الدخول
+            </Button>
+          </form>
+        </TabsContent>
       </Tabs>
     </main>
   );
