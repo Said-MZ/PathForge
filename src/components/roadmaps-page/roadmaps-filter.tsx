eec73eb3d3c03674921990cac9a561249ffd2d57
Said zain <saidzain897@gmail.com>
2024-03-29 01:54:13 +0300

    Fetch categories

--- src/components/roadmaps-page/roadmaps-filter.tsx
+++ src/components/roadmaps-page/roadmaps-filter.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
@@ -17,71 +17,72 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
-const roadmaps = [
-  { id: 1, label: "اللغة العربية", value: "اللغة العربية" },
-  { id: 2, label: "الرياضيات", value: "الرياضيات" },
-  { id: 3, label: "العلوم", value: "العلوم" },
-  { id: 4, label: "التاريخ", value: "التاريخ" },
-  { id: 5, label: "الجغرافيا", value: "الجغرافيا" },
-  { id: 6, label: "الفيزياء", value: "الفيزياء" },
-];
-
 export function RoadmapsFilter() {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState<string>("");
-  const [selectedId, setSelectedId] = useState(0);
+  const [name, setName] = useState("all");
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
 
+  const getCategories = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://hackthon-ysxk.onrender.com/categories"
+      );
+      const data = await response.json();
+      setCategories(data.data);
+    } catch (error: any) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
   return (
-    <div className="flex gap-4 justify-center items-center">
-      <Button
-        variant="default"
-        className="w-[200px]"
-        onClick={() => {
-          console.log("value", value);
-          console.log("selectedId", selectedId);
-        }}
-      >
-        تصفية
-      </Button>
+    <div className="flex gap-4 justify-center items-center z-20">
       <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger
-          asChild
-          className="flex flex-row-reverse justify-stretch items-center"
-        >
+        <PopoverTrigger asChild>
           <Button
             variant="outline"
             role="combobox"
             aria-expanded={open}
-            className="w-[200px] justify-between pr-2 pl-0"
+            className="w-[200px] justify-between"
           >
-            {value
-              ? roadmaps.find((framework) => framework.value === value)?.label
-              : "الكل"}
-
+            {name ? name : "Select category..."}
             <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-[200px] p-0">
           <Command>
-            <CommandInput placeholder="ابحث عن خريطة تعلم" className="h-9" />
-            <CommandEmpty>لا يوجد نتائج للبحث</CommandEmpty>
+            <CommandInput placeholder="Search framework..." className="h-9" />
+            <CommandEmpty>No framework found.</CommandEmpty>
             <CommandGroup>
-              {roadmaps.map((framework) => (
+              {categories.map((category: any) => (
                 <CommandItem
-                  key={framework.value}
-                  value={framework.value}
+                  key={category.name}
+                  value={category.name}
                   onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue);
+                    setName(currentValue === name ? "" : currentValue);
+                    setSelectedCategory(category.id);
                     setOpen(false);
-                    setSelectedId(framework.id);
                   }}
-                  className="flex flex-row-reverse justify-between items-between w-full"
                 >
-                  {framework.label}
+                  {category.name}
                   <CheckIcon
                     className={cn(
-                      "h-4 w-4",
-                      value === framework.value ? "opacity-100" : "opacity-0"
+                      "ml-auto h-4 w-4",
+                      name.toLocaleLowerCase() === category.name.toLowerCase()
+                        ? "opacity-100"
+                        : "opacity-0"
                     )}
                   />
                 </CommandItem>
